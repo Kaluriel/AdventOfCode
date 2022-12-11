c23e585f3d9bf70b2fbe3fe876608cbaca4a6c7d
Kaluriel <kaluriel+github@gmail.com>
2022-12-11 10:23:34 +0000

    updated day 11

--- AdventOfCode/Days/Y2022/Day11.cs
+++ AdventOfCode/Days/Y2022/Day11.cs
@@ -44,31 +44,13 @@ namespace AdventOfCode.Days.Y2022
 			
 			for (int r = 0; r < 20; ++r)
 			{
-				for (int m = 0; m < monkeyData.Length; ++m)
-				{
-					monkeyData[m].ItemsInspected += (UInt64)monkeyData[m].Items.Count;
-
-					for (int i = 0; i < monkeyData[m].Items.Count; ++i)
-					{
-						monkeyData[m].Items[i] = ParseExpression(monkeyData[m].Monkey, monkeyData[m].Items[i]) / 3;
-						int targetMonkey = monkeyData[m].Monkey.TestFalseMonkey;
-
-						if ((monkeyData[m].Items[i] % monkeyData[m].Monkey.TestConstant) == 0)
-						{
-							targetMonkey = monkeyData[m].Monkey.TestTrueMonkey;
-						}
-
-						monkeyData[targetMonkey].Items.Add(monkeyData[m].Items[i]);
-					}
-
-					monkeyData[m].Items.Clear();
-				}
+				RunMonkeys(monkeyData, 3, true);
 			}
 
 			return Task.FromResult<object>(
 				monkeyData.OrderByDescending(x => x.ItemsInspected)
-						  .Select(x => x.ItemsInspected)
 						  .Take(2)
+						  .Select(x => x.ItemsInspected)
 						  .Aggregate((x, y) => x * y)
 			);
 		}
@@ -83,33 +65,50 @@ namespace AdventOfCode.Days.Y2022
 
 			for (int r = 0; r < 10000; ++r)
 			{
-				for (int m = 0; m < monkeyData.Length; ++m)
-				{
-					monkeyData[m].ItemsInspected += (UInt64)monkeyData[m].Items.Count;
-
-					for (int i = 0; i < monkeyData[m].Items.Count; ++i)
-					{
-						monkeyData[m].Items[i] = ParseExpression(monkeyData[m].Monkey, monkeyData[m].Items[i]) % lcm;
-						int targetMonkey = monkeyData[m].Monkey.TestFalseMonkey;
-
-						if ((monkeyData[m].Items[i] % monkeyData[m].Monkey.TestConstant) == 0)
-						{
-							targetMonkey = monkeyData[m].Monkey.TestTrueMonkey;
-						}
-
-						monkeyData[targetMonkey].Items.Add(monkeyData[m].Items[i]);
-					}
-
-					monkeyData[m].Items.Clear();
-				}
+				RunMonkeys(monkeyData, lcm, false);
 			}
 
 			return Task.FromResult<object>(
 				monkeyData.OrderByDescending(x => x.ItemsInspected)
-						  .Select(x => x.ItemsInspected)
 						  .Take(2)
+						  .Select(x => x.ItemsInspected)
 						  .Aggregate((x, y) => x * y)
 			);
+		}
+
+		private void RunMonkeys(MonkeyData[] monkeyData, UInt64 worryReducer, bool useDivide)
+		{
+			// for each monkey
+			for (int m = 0; m < monkeyData.Length; ++m)
+			{
+				// for each item
+				for (int i = 0; i < monkeyData[m].Items.Count; ++i)
+				{
+					// change worry level
+					monkeyData[m].Items[i] = ParseExpression(monkeyData[m].Monkey, monkeyData[m].Items[i]);
+
+					// reduce worry
+					if (useDivide)
+					{
+						monkeyData[m].Items[i] /= worryReducer;
+					}
+					else
+					{
+						monkeyData[m].Items[i] %= worryReducer;
+					}
+
+					// move item to target monkey
+					int targetMonkey = ((monkeyData[m].Items[i] % monkeyData[m].Monkey.TestConstant) == 0)
+						? monkeyData[m].Monkey.TestTrueMonkey
+						: monkeyData[m].Monkey.TestFalseMonkey;
+
+					monkeyData[targetMonkey].Items.Add(monkeyData[m].Items[i]);
+				}
+
+				// increment items inspected and reset item list
+				monkeyData[m].ItemsInspected += (UInt64)monkeyData[m].Items.Count;
+				monkeyData[m].Items.Clear();
+			}
 		}
 
 		// this is just a sequence, it doesn't have an order of operations
