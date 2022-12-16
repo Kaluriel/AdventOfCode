930500b8955ed15a2e579e81977d3e7e65ff1fd6
Kaluriel <kaluriel+github@gmail.com>
2022-12-16 01:46:15 +0000

    added new data types

---
+++ AdventOfCode/DataTypes/Area2D.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.DataTypes
+{
+	public struct Area2D
+	{
+		public Point2D Min { get; set; }
+		public Point2D Max { get; set; }
+
+		public Area2D(Point2D min, Point2D max)
+		{
+			Min = min;
+			Max = max;
+		}
+
+		public IEnumerable<Point2D> GetCorners()
+		{
+			yield return Min;
+			yield return new Point2D(Min.X, Max.Y);
+			yield return Max;
+			yield return new Point2D(Max.X, Min.Y);
+		}
+
+		public Point2D Size => Max - Min;
+		public bool IsZero => Size == Point2D.Zero;
+		public bool IsOne => Size == Point2D.One;
+
+		public IEnumerable<Area2D> Subdivide()
+		{
+			Point2D halfSize = Size / 2;
+			
+			if (halfSize.X == 0 && halfSize.Y != 0)
+			{
+				halfSize.X = 1;
+			}
+			else if (halfSize.Y == 0 && halfSize.X != 0)
+			{
+				halfSize.Y = 1;
+			}
+
+			Point2D mid = Min + halfSize;
+
+			yield return new Area2D(Min, mid);
+
+			yield return new Area2D(
+				new Point2D(mid.X + 1, Min.Y),
+				new Point2D(Max.X, mid.Y)
+			);
+
+			yield return new Area2D(
+				new Point2D(Min.X, mid.Y + 1),
+				new Point2D(mid.X, Max.Y)
+			);
+
+			yield return new Area2D(
+				mid + new Point2D(1, 1),
+				Max
+			);
+		}
+
+		public bool Overlaps(Area2D area)
+		{
+			bool xOverlap = (this.Min.X >= area.Min.X) && (this.Min.X <= area.Max.X) &&
+							(area.Min.X >= this.Min.X) && (area.Min.X <= this.Max.X);
+
+			bool yOverlap = (this.Min.Y >= area.Min.Y) && (this.Min.Y <= area.Max.Y) &&
+							(area.Min.Y >= this.Min.Y) && (area.Min.Y <= this.Max.Y);
+
+			return xOverlap && yOverlap;
+		}
+	}
+}
--- AdventOfCode/DataTypes/Point2D.cs
+++ AdventOfCode/DataTypes/Point2D.cs
@@ -22,6 +22,11 @@ namespace AdventOfCode.DataTypes
 			Y = int.Parse(parts[1]);
 		}
 
+		public static Point2D operator /(Point2D p1, int denom)
+		{
+			return new Point2D(p1.X / denom, p1.Y / denom);
+		}
+
 		public static Point2D operator +(Point2D p1, Point2D p2)
 		{
 			return new Point2D(p1.X + p2.X, p1.Y + p2.Y);
@@ -50,15 +55,22 @@ namespace AdventOfCode.DataTypes
 			);
 		}
 
+		public int GetManhattenDistance(Point2D other)
+		{
+			return Math.Abs(this.X - other.X) + Math.Abs(this.Y - other.Y);
+		}
+
 		public override string ToString()
 		{
 			return $"{X}, {Y}";
 		}
 
-		public static readonly Point2D up = new Point2D(0, -1);
-		public static readonly Point2D down = new Point2D(0, 1);
-		public static readonly Point2D left = new Point2D(-1, 0);
-		public static readonly Point2D right = new Point2D(1, 0);
+		public static readonly Point2D Zero = new Point2D(0, 0);
+		public static readonly Point2D One = new Point2D(1, 1);
+		public static readonly Point2D Up = new Point2D(0, -1);
+		public static readonly Point2D Down = new Point2D(0, 1);
+		public static readonly Point2D Left = new Point2D(-1, 0);
+		public static readonly Point2D Right = new Point2D(1, 0);
 
 		public override bool Equals(object? obj)
 		{
@@ -77,10 +89,10 @@ namespace AdventOfCode.DataTypes
 
 		public static readonly IReadOnlyList<Point2D> OrthoDirections = new Point2D[]
 		{
-			Point2D.up,
-			Point2D.down,
-			Point2D.left,
-			Point2D.right,
+			Point2D.Up,
+			Point2D.Down,
+			Point2D.Left,
+			Point2D.Right,
 		};
 	}
 }
