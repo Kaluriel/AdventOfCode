6f1481e45b864b82a08fb9dc71ef8c989994e11d
Kaluriel <steve@nearlight.co.uk>
2024-12-09 12:53:04 +0000

    day 9

--- AdventOfCode/Days/Y2024/Day09.cs
+++ AdventOfCode/Days/Y2024/Day09.cs
@@ -14,33 +14,103 @@ namespace AdventOfCode.Days.Y2024
 {
 	public class Day09 : DayBase2024
 	{
-		protected override Task ExecuteSharedAsync()
+		private struct Block
 		{
-			return base.ExecuteSharedAsync();
+			public int File { get; set; }
+			public int Count { get; set; }
 		}
 
 		protected override Task<object> ExecutePart1Async(int testIndex)
 		{
+			var fileSystem = Source
+					.SelectMany(
+						(f, i) => Enumerable.Repeat<Int64>(
+							((i & 1) == 0)
+								? i / 2
+								: -1,
+							f - '0'
+						)
+					)
+					.ToList();
+
+			for (int i = 0; i < fileSystem.Count; ++i)
+			{
+				if (fileSystem[i] == -1)
+				{
+					int lastIndex = fileSystem.FindLastIndex(x => x != -1);
+					if ((lastIndex == -1) || (i >= lastIndex))
+					{
+						break;
+					}
+
+					(fileSystem[i], fileSystem[lastIndex]) = (fileSystem[lastIndex], fileSystem[i]);
+				}
+			}
+			
 			return Task.FromResult<object>(
-				NumUniqueAntinodeLocations()
+				CalculateChecksum(
+					fileSystem
+				)
 			);
 		}
 
 		protected override Task<object> ExecutePart2Async(int testIndex)
 		{
+			var fileSystem = Source
+				.Select(
+					(f, i) => new Block
+					{
+						File = ((i & 1) == 0)
+							? i / 2
+							: -1,
+						Count = f - '0',
+					}
+				)
+				.Where(f => f.Count > 0)
+				.ToList();
+			
+			for (int i = fileSystem.Count - 1; i >= 0; --i)
+			{ 
+				if (fileSystem[i].File != -1)
+				{
+					int blockCount = fileSystem[i].Count;
+
+					int index = fileSystem.FindIndex(x => (x.File == -1) && (x.Count >= blockCount));
+					if ((index == -1) || (index >= i))
+					{
+						continue;
+					}
+
+					int freeBlockSize = fileSystem[index].Count;
+
+					fileSystem[index] = fileSystem[i];
+					fileSystem[i] = new Block
+					{
+						File = -1,
+						Count = blockCount,
+					};
+
+					if (freeBlockSize > blockCount)
+					{
+						fileSystem.Insert(index + 1, new Block { File = -1, Count = freeBlockSize - blockCount });
+						++i;
+					}
+				}
+			}
+			
 			return Task.FromResult<object>(
-				NumUniqueAntinodeLocationsIgnoreDistance()
+				CalculateChecksum(
+					fileSystem.SelectMany(x => Enumerable.Repeat<Int64>(x.File, x.Count))
+				)
 			);
 		}
 
-		private int NumUniqueAntinodeLocations()
+		private Int64 CalculateChecksum(IEnumerable<Int64> values)
 		{
-			return 1;
-		}
-
-		private int NumUniqueAntinodeLocationsIgnoreDistance()
-		{
-			return 1;
+			return values
+				.Select((f, i) => f * i)
+				.Where(x => x >= 0)
+				.Sum();
 		}
 	}
 }
