ad9c696ffb6664ecd0284825c2294cc14b1c42d6
Kaluriel <steve@nearlight.co.uk>
2024-12-12 17:03:53 +0000

    day 12

---
+++ AdventOfCode/Days/Y2024/Day12.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using AdventOfCode.DataTypes;
+using AdventOfCode.Ext;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Days.Y2024
+{
+	public sealed class Day12 : Day
+	{
+		private char[][] GardenMap;
+		private List<(char, HashSet<Point2D>)> Regions = new List<(char, HashSet<Point2D>)>();
+
+		protected override Task ExecuteSharedAsync()
+		{
+			GardenMap = CreateCharMapFromSource(Source)
+				.Select(x => x.ToArray())
+				.ToArray();
+			
+			Dictionary<char, HashSet<Point2D>> plantLists = new Dictionary<char, HashSet<Point2D>>();
+
+			for (int y = 0; y < GardenMap.Length; ++y)
+			{
+				for (int x = 0; x < GardenMap[y].Length; ++x)
+				{
+					char regionId = GardenMap[y][x];
+					var point = new Point2D(x, y);
+
+					if (plantLists.ContainsKey(regionId))
+					{
+						plantLists[regionId].Add(point);
+					}
+					else
+					{
+						plantLists.Add(regionId, [point]);
+					}
+				}
+			}
+			
+			Regions = new List<(char, HashSet<Point2D>)>();
+
+			foreach (var plantList in plantLists)
+			{
+				while (plantList.Value.Count > 0)
+				{
+					var pointList = new List<Point2D>()
+					{
+						plantList.Value.First()
+					};
+					
+					// first entry
+					plantList.Value.Remove(pointList[0]);
+
+					// find adjacent points
+					for (int index = 0; index < pointList.Count; ++index)
+					{
+						for (int i = 0; i < Point2D.OrthoDirections.Count; ++i)
+						{
+							Point2D adjPoint = pointList[index] + Point2D.OrthoDirections[i];
+							if (plantList.Value.Contains(adjPoint))
+							{
+								plantList.Value.Remove(adjPoint);
+								pointList.Add(adjPoint);
+							}
+						}
+					}
+
+					Regions.Add((plantList.Key, [..pointList]));
+				}
+			}
+
+			return base.ExecuteSharedAsync();
+		}
+
+		protected override Task<object> ExecutePart1Async(int testIndex)
+		{
+			return Task.FromResult<object>(
+				TotalCostOfFencing()
+			);
+		}
+
+		protected override Task<object> ExecutePart2Async(int testIndex)
+		{
+			return Task.FromResult<object>(
+				TotalCostOfFencingWithBulkDiscount()
+			);
+		}
+
+		private Int64 TotalCostOfFencing()
+		{
+			return Regions.Sum(region => region.Item2.Count * MathExt.CalculatePerimeter(region.Item2, true));
+		}
+
+		private Int64 TotalCostOfFencingWithBulkDiscount()
+		{
+			return Regions.Sum(region => region.Item2.Count * MathExt.CountEdges(region.Item2, true));
+		}
+	}
+}
--- AdventOfCode/Ext/MathExt.cs
+++ AdventOfCode/Ext/MathExt.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AdventOfCode.DataTypes;
 
 namespace AdventOfCode.Ext
@@ -45,6 +44,144 @@ namespace AdventOfCode.Ext
 		public static int DigitCount(double value)
 		{
 			return (int)Math.Floor(Math.Log10(value) + 1);
+		}
+		
+		public static int CalculatePerimeter(HashSet<Point2D> points, bool includeInternal)
+		{
+			return InternalGetOrthoEdgePoints(points, includeInternal).Count;
+		}
+		
+		public static int CountEdges(HashSet<Point2D> points, bool includeInternal)
+		{
+			var edgePoints = InternalGetOrthoEdgePoints(points, includeInternal).ToList();
+			var connectedEdges = new List<Point2D>();
+
+			for (int i = 0; i < edgePoints.Count; ++i)
+			{
+				int directionIndex = edgePoints[i].Item1;
+				Point2D point = edgePoints[i].Item2;
+				
+				// clear previous edges
+				connectedEdges.Clear();
+				
+				// add this as the initial edge
+				connectedEdges.Add(point);
+				edgePoints.RemoveAt(i);
+
+				// remove edges adjacent to this one
+				for (int j = 0; j < connectedEdges.Count; ++j)
+				{
+					switch (directionIndex)
+					{
+						// edge goes up or down
+						case 0:
+						case 2:
+							{
+								// check right
+								var p1 = (directionIndex, connectedEdges[j] + Point2D.OrthoDirections[1]);
+								if (edgePoints.Contains(p1))
+								{
+									connectedEdges.Add(p1.Item2);
+									edgePoints.Remove(p1);
+								}
+
+								// check left
+								var p2 = (directionIndex, connectedEdges[j] + Point2D.OrthoDirections[3]);
+								if (edgePoints.Contains(p2))
+								{
+									connectedEdges.Add(p2.Item2);
+									edgePoints.Remove(p2);
+								}
+							}
+							break;
+						
+						// edge goes right or left
+						case 1:
+						case 3:
+							{
+								// check above
+								var p1 = (directionIndex, connectedEdges[j] + Point2D.OrthoDirections[0]);
+								if (edgePoints.Contains(p1))
+								{
+									connectedEdges.Add(p1.Item2);
+									edgePoints.Remove(p1);
+								}
+
+								// check below
+								var p2 = (directionIndex, connectedEdges[j] + Point2D.OrthoDirections[2]);
+								if (edgePoints.Contains(p2))
+								{
+									connectedEdges.Add(p2.Item2);
+									edgePoints.Remove(p2);
+								}
+							}
+							break;
+					}
+				}
+				
+				// readd this edge so we can use it in our results
+				edgePoints.Insert(i, (directionIndex, point));
+			}
+
+			return edgePoints.Count;
+		}
+		
+		private static HashSet<(int, Point2D)> InternalGetOrthoEdgePoints(HashSet<Point2D> points, bool includeInternal)
+		{
+			HashSet<(int, Point2D)> visited = new HashSet<(int, Point2D)>();
+
+			if (points.Count > 0)
+			{
+				if (includeInternal)
+				{
+					foreach (var p in points)
+					{
+						for (int i = 0; i < Point2D.OrthoDirections.Count; ++i)
+						{
+							var orthoDirection = Point2D.OrthoDirections[i];
+
+							if (!points.Contains(p + orthoDirection))
+							{
+								visited.Add((i, p));
+							}
+						}
+					}
+				}
+				else
+				{
+					Point2D point = points.First();
+					int directionIndex = 0;
+
+					foreach (var p in points)
+					{
+						if (p.Y < point.Y)
+						{
+							directionIndex = 0;
+							point = p;
+						}
+					}
+
+					while (!visited.Contains((directionIndex, point)))
+					{
+						Point2D inFrontPoint = point + Point2D.OrthoDirections[directionIndex];
+						
+						if (points.Contains(inFrontPoint))
+						{
+							point = inFrontPoint;
+							directionIndex += Point2D.OrthoDirections.Count - 1;
+						}
+						else
+						{
+							visited.Add((directionIndex, point));
+							++directionIndex;
+						}
+						
+						directionIndex %= Point2D.OrthoDirections.Count;
+					}
+				}
+			}
+
+			return visited;
 		}
 	}
 }
