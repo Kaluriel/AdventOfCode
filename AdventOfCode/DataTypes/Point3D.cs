93b6ad194194a82ae5f2999173d53d454446e641
Kaluriel <kaluriel@gmail.com>
2025-12-08 16:03:57 +0000

    day 8

---
+++ AdventOfCode/DataTypes/Point3D.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.DataTypes
+{
+	public struct Point3D
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Z { get; set; }
+
+		public Point3D(int[] xyz)
+		{
+			_ = xyz ?? throw new ArgumentNullException(nameof(xyz));
+			if (xyz.Length != 3)
+			{
+				throw new ArgumentException("Points must be of 3 elements.", nameof(xyz));
+			}
+			X = xyz[0];
+			Y = xyz[1];
+			Z = xyz[2];
+		}
+
+		public Point3D(int x, int y, int z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		public Point3D(string str)
+		{
+			var parts = str.Split([',']);
+			X = int.Parse(parts[0]);
+			Y = int.Parse(parts[1]);
+			Z = int.Parse(parts[2]);
+		}
+
+		public static Point3D operator -(Point3D p1)
+		{
+			return new Point3D(-p1.X, -p1.Y, -p1.Z);
+		}
+
+		public static Point3D operator /(Point3D p1, int denom)
+		{
+			return new Point3D(p1.X / denom, p1.Y / denom, p1.Z / denom);
+		}
+
+		public static Point3D operator %(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.X % p2.X, p1.Y % p2.Y, p1.Z % p2.Z);
+		}
+
+		public static Point3D operator %(Point3D p1, int p2)
+		{
+			return new Point3D(p1.X % p2, p1.Y % p2, p1.Z % p2);
+		}
+
+		public static Point3D operator +(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+		}
+
+		public static Point3D operator -(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z);
+		}
+
+		public static Point3D operator *(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.X * p2.X, p1.Y * p2.Y, p1.Z * p2.Z);
+		}
+
+		public static Point3D operator *(Point3D p1, int mul)
+		{
+			return new Point3D(p1.X * mul, p1.Y * mul, p1.Z * mul);
+		}
+
+		public static Point3D operator *(int mul, Point3D p1)
+		{
+			return new Point3D(p1.X * mul, p1.Y * mul, p1.Z * mul);
+		}
+
+		public static bool operator ==(Point3D p1, Point3D p2)
+		{
+			return (p1.X == p2.X) && (p1.Y == p2.Y) && (p1.Z == p2.Z);
+		}
+
+		public static bool operator ==(Point3D p1, int p2)
+		{
+			return (p1.X == p2) && (p1.Y == p2) && (p1.Z == p2);
+		}
+
+		public static bool operator !=(Point3D p1, Point3D p2)
+		{
+			return (p1.X != p2.X) || (p1.Y != p2.Y) || (p1.Z != p2.Z);
+		}
+
+		public static bool operator !=(Point3D p1, int p2)
+		{
+			return (p1.X != p2) || (p1.Y != p2) || (p1.Z != p2);
+		}
+
+		public static bool operator <(Point3D p1, int p2)
+		{
+			return (p1.X < p2) && (p1.Y < p2) && (p1.Z < p2);
+		}
+
+		public static bool operator >(Point3D p1, int p2)
+		{
+			return (p1.X > p2) && (p1.Y > p2) && (p1.Z > p2);
+		}
+
+		public static bool operator <=(Point3D p1, int p2)
+		{
+			return (p1.X <= p2) && (p1.Y <= p2) && (p1.Z <= p2);
+		}
+
+		public static bool operator >=(Point3D p1, int p2)
+		{
+			return (p1.X >= p2) && (p1.Y >= p2) && (p1.Z >= p2);
+		}
+
+		public Point3D Sign()
+		{
+			return new Point3D(
+				Math.Sign(X),
+				Math.Sign(Y),
+				Math.Sign(Z)
+			);
+		}
+
+		public float GetDistanceSquared(Point3D other)
+		{
+			return (float)(Math.Pow(this.X - other.X, 2) + Math.Pow(this.Y - other.Y, 2) + Math.Pow(this.Z - other.Z, 2));
+		}
+
+		public float GetDistance(Point3D other)
+		{
+			return (float)Math.Sqrt(GetDistanceSquared(other));
+		}
+
+		public int GetManhattenDistance(Point3D other)
+		{
+			return Math.Abs(this.X - other.X) + Math.Abs(this.Y - other.Y) + Math.Abs(this.Z - other.Z);
+		}
+
+		public bool IsAdjacent(Point3D point)
+		{
+			return (Math.Abs(point.X - X) <= 1) && (Math.Abs(point.Y - Y) <= 1) && (Math.Abs(point.Z - Z) <= 1);
+		}
+
+		public override string ToString()
+		{
+			return $"{X}, {Y}, {Z}";
+		}
+
+		public override bool Equals(object? obj)
+		{
+			if (base.Equals(obj))
+			{
+				return this == ((Point3D)obj);
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return ToString().GetHashCode();
+		}
+	}
+}
---
+++ AdventOfCode/Days/Y2025/Day08.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using AdventOfCode.DataTypes;
+using AdventOfCode.Ext;
+
+namespace AdventOfCode.Days.Y2025
+{
+	public sealed class Day08 : Day
+	{
+		private Point3D[] _Points;
+
+		protected override async Task ExecuteSharedAsync()
+		{
+			await base.ExecuteSharedAsync();
+			
+			_Points = Source
+				.SplitNewLine(StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => new  Point3D(x))
+				.ToArray();
+		}
+
+		protected override Task<object> ExecutePart1Async(int testIndex)
+		{
+			var junctionCircuit = new Dictionary<Point3D, HashSet<Point3D>>();
+			var closestPoints = new List<(Point3D, Point3D, float)>();
+			
+			// determine distances
+			for (int p1 = 0; p1 < _Points.Length; ++p1)
+			{
+				for (int p2 = p1 + 1; p2 < _Points.Length; ++p2)
+				{
+					float distance = _Points[p1].GetDistance(_Points[p2]);
+					closestPoints.Add(
+						(_Points[p1], _Points[p2], distance)
+					);
+				}
+			}
+
+			// sort for the closest
+			closestPoints.Sort((a, b) => a.Item3.CompareTo(b.Item3));
+			
+			// limit how many we're checking
+#if TEST
+			closestPoints.RemoveRange(10, closestPoints.Count - 10);
+#else
+			closestPoints.RemoveRange(1000, closestPoints.Count - 1000);
+#endif
+
+			// prepopulate so we save a bunch of unnecessary null checks
+			foreach (var point in _Points)
+			{
+				junctionCircuit.Add(point, [point]);
+			}
+
+			foreach (var closestPair in closestPoints)
+			{
+				var circuit1 = junctionCircuit[closestPair.Item1];
+				var circuit2 = junctionCircuit[closestPair.Item2];
+
+				// already joined?
+				if (circuit1 == circuit2)
+				{
+					continue;
+				}
+
+				// combine circuits
+				circuit1.UnionWith(circuit2);
+
+				// update dictionary with the new parent circuit
+				foreach (var point in circuit2)
+				{
+					junctionCircuit[point] = circuit1;
+				}
+			}
+
+			return Task.FromResult<object>(
+				junctionCircuit.Values.Distinct()
+					.OrderByDescending(x => x.Count)
+					.Take(3)
+					.Aggregate(1, (x, y) => x * y.Count)
+			);
+		}
+
+		protected override Task<object> ExecutePart2Async(int testIndex)
+		{
+			var junctionCircuit = new Dictionary<Point3D, HashSet<Point3D>>();
+			var closestPoints = new List<(Point3D, Point3D, float)>();
+			int lastTwoXProduct = 0;
+			
+			// determine distances
+			for (int p1 = 0; p1 < _Points.Length; ++p1)
+			{
+				for (int p2 = p1 + 1; p2 < _Points.Length; ++p2)
+				{
+					float distance = _Points[p1].GetDistance(_Points[p2]);
+					closestPoints.Add(
+						(_Points[p1], _Points[p2], distance)
+					);
+				}
+			}
+
+			// sort for the closest
+			closestPoints.Sort((a, b) => a.Item3.CompareTo(b.Item3));
+
+			// prepopulate so we save a bunch of unnecessary null checks
+			foreach (var point in _Points)
+			{
+				junctionCircuit.Add(point, [point]);
+			}
+
+			foreach (var closestPair in closestPoints)
+			{
+				var circuit1 = junctionCircuit[closestPair.Item1];
+				var circuit2 = junctionCircuit[closestPair.Item2];
+
+				// already joined?
+				if (circuit1 == circuit2)
+				{
+					continue;
+				}
+
+				// combine circuits
+				circuit1.UnionWith(circuit2);
+
+				// update dictionary with the new parent circuit
+				foreach (var point in circuit2)
+				{
+					junctionCircuit[point] = circuit1;
+				}
+
+				// keep calculating the product so it's eventually left on the product of the last two
+				lastTwoXProduct = closestPair.Item1.X * closestPair.Item2.X;
+			}
+
+			return Task.FromResult<object>(lastTwoXProduct);
+		}
+	}
+}
